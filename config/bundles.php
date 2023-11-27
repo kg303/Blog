0a52d0b332715d457d44f18b619f854a73507527
Dejan C <dejan.crnomarkovic@asioso.com>
2023-11-27 11:13:24 +0100

    initial commit

---
+++ config/bundles.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
+    Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle::class => ['all' => true],
+    Pimcore\Bundle\GlossaryBundle\PimcoreGlossaryBundle::class => ['all' => true],
+    Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
+    Pimcore\Bundle\StaticRoutesBundle\PimcoreStaticRoutesBundle::class => ['all' => true],
+    Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
+    Pimcore\Bundle\UuidBundle\PimcoreUuidBundle::class => ['all' => true],
+    Pimcore\Bundle\WordExportBundle\PimcoreWordExportBundle::class => ['all' => true],
+    Pimcore\Bundle\XliffBundle\PimcoreXliffBundle::class => ['all' => true],
+];
